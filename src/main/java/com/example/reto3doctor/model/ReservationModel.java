4d28b8de7ac06e88c5640e71a65af2ba29a49cab
cjaf1994 <cristian.aricapa.mt@usa.edu.co>
2022-10-13 10:45:10 -0400

    Actualizar localDate---Date

--- src/main/java/com/example/reto3doctor/model/ReservationModel.java
+++ src/main/java/com/example/reto3doctor/model/ReservationModel.java
@@ -21,8 +21,8 @@ public class ReservationModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idReservation;
-    private LocalDate startDate;
-    private LocalDate devolutionDate;
+    private Date startDate;
+    private Date devolutionDate;
     private String status= "created";
     @ManyToOne()
     @JoinColumn(name = "id_doctor")
